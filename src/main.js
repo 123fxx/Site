e0b6affd64dfe220225de4e35ba37dd05b662f8e
fengpt <1378247557@qq.com>
2020-09-15 08:57:50 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+
 import App from './App'
 import router from './router'
 import 'babel-polyfill'
@@ -9,26 +10,48 @@ import {
   post,
   useAppend,
 } from "./utils/http";
-import VueQuillEditor from 'vue-quill-editor'
+import store from './utils/store'
+// import VueQuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
 
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import echarts from 'echarts' //引入echarts
-Vue.use(ElementUI);
-Vue.use(VueQuillEditor)
-Vue.config.productionTip = false
+import "@/assets/css/reset.css";
+import epfTitle from '@/components/title'
+import epfNav from '@/components/nav'
+import scroll from 'vue-seamless-scroll'
 
+
+import epfPage from '@/components/page'
+import echarts from 'echarts' //引入echarts
+// import Print from 'vue-print-nb'
+import Bus from './utils/bus';
+import Print from '@/assets/js/print'
+// Vue.use(Print) // 注册
+Vue.component("epfTitle", epfTitle);
+Vue.component("epfNav", epfNav);
+Vue.component("epfPage", epfPage);
+Vue.use(ElementUI);
+// Vue.use(VueQuillEditor)
+Vue.config.productionTip = false
+Vue.use(scroll)
+Vue.use(Print);
 Vue.prototype.$get = get;
 Vue.prototype.$post = post;
 Vue.prototype.$useAppend = useAppend;
-Vue.prototype.$echarts = echarts //挂载在原型，组件内使用直接this.$echarts调用
+Vue.prototype.$echarts = echarts
+Vue.prototype.$bus = Bus
+
+
+
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  store
 })
